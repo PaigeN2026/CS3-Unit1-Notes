cb8322a5611795f85e209f009a3ebba40b824208
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-09-16 19:08:45 +0000

    created lists

--- main.py
+++ main.py
@@ -46,3 +46,43 @@ def greet(name):
     print(f"Hey there, {name}!!!")
 greet("Natalie")
 greet(3.14)
+
+# ** LISTS **
+# ordered, mutable, sequences 
+empty_list = list() # constructor
+another_empty_list = []
+
+class_roster = ["Bryce", "Finny", "Jackson", "Kevin", "Maia", "Natalie", "Paige"]
+print(class_roster)
+print(len(class_roster))
+
+# Indexes start at 0 
+first_item = class_roster[0]
+print(first_item)
+
+# Update an item in a list, access by index
+class_roster[2] = "Jack"
+print(class_roster)
+
+# Sorting lists 
+lottery_nums = [5,148, 12, 589, 1, 999, 32039458, 51]
+print(sorted(lottery_nums))
+print(sorted(lottery_nums, reverse=True))
+print(lottery_nums) # sorted() does not modify OG list
+# Sort IN PLACE -> modifies OG list
+lottery_nums.sort()
+print(lottery_nums)
+class_roster.sort(reverse=True)
+print(class_roster)
+
+# List operations
+class_roster.append("Alex")
+class_roster.insert(0, "Zoie")
+print(class_roster)
+class_roster.remove("Zoie")
+class_roster.pop() # remove last item
+print(class_roster)
+
+# Check if item exists in a list
+print(13 in lottery_nums)
+print(1 in lottery_nums)
