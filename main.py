9f182d5fa9e8e581a5ba1aeecf66d717cf3affd7
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-09-17 13:42:01 +0000

    created tuples, sets, and dictionaries

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ def greet(name):
 greet("Natalie")
 greet(3.14)
 
+print(" ")
+
 # ** LISTS **
 # ordered, mutable, sequences 
 empty_list = list() # constructor
@@ -64,6 +66,8 @@ print(first_item)
 class_roster[2] = "Jack"
 print(class_roster)
 
+print(" ")
+
 # Sorting lists 
 lottery_nums = [5,148, 12, 589, 1, 999, 32039458, 51]
 print(sorted(lottery_nums))
@@ -72,6 +76,9 @@ print(lottery_nums) # sorted() does not modify OG list
 # Sort IN PLACE -> modifies OG list
 lottery_nums.sort()
 print(lottery_nums)
+
+print(" ")
+
 class_roster.sort(reverse=True)
 print(class_roster)
 
@@ -83,6 +90,56 @@ class_roster.remove("Zoie")
 class_roster.pop() # remove last item
 print(class_roster)
 
+print(" ")
+
 # Check if item exists in a list
 print(13 in lottery_nums)
 print(1 in lottery_nums)
+
+print(" ")
+
+# ** Tuples **
+# ordered & immutable (can't change items)
+# useful for "snapshot" of a row of data
+student = ('Paige', 17, 'Post-AP Comp Sci', 4.0)
+print(student)
+# student[3] = 2.6 # CAN'T RE-ASSIGN ITEM!
+
+print(" ")
+
+# ** SETS **
+# unsorted, stores other immutable types
+# NO DUPLICATES allowed 
+songs = {'Stranger', '3005', '7', '3', 'Mutt', 'Freeze', '3005'}
+print(songs)
+# sets can be used to de-duplicate list items
+colors = ['red', 'pink', 'orange', 'yellow', 'blue', 'purple', 'pink']
+print(set(colors))
+# you CAN add/remove items
+songs.add('Nobody New')
+songs.add('Stranger') # duplicate value 
+print(songs)
+# look up set OPERATIONS to compare items between sets
+
+print(" ")
+
+# ** DICTIONARIES **
+# mutable, but the KEYS can only be immutable types 
+# { key: value } pairs. Keys must be UNIQUE
+# unordered (no index, can't sort in place)
+characters =  {'Aelin': 'assassin queen', 
+               'Karate Kid': 'pupil',
+               'Mr. Miyagi': 'sensei',
+               'Phil Dunphy': 'dad',
+               'Wall-E': 'trash robot', 
+               'Princess Peach': 'damsel in distress',
+               'Dexter': 'serial killer (justified?)',
+               'Lara Jean': 'letter writer'
+               }
+print(len(characters))
+# disctionary with numerical keys, list valued
+grade_requirements = { 9: ['Bio', 'Math', 'English', 'History', 'PE'],
+                      10: ['Chem', 'Math', 'English', 'History', 'PE'],
+                      11:['Physics', 'Math', 'English', 'History', 'PE'],
+                      12: ['Math', 'English', 'PE']
+                      }
